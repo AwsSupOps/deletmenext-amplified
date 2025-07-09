c1a04d7f856e7a8e53255cfeadd9618fd21bf8de
EC2 Default User <ec2-user@ip-172-31-7-155.ec2.internal>
2025-07-09 00:42:36 +0000

    first commit1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,11 +14,10 @@ export default function Home() {
           priority
         />
 
-        
-        
+      
         <ol>
           <li>
-            Get started by editing <code>src/app/page.tsx</code>.
+            Get started by editing 1 <code>src/app/page.tsx</code>.
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
