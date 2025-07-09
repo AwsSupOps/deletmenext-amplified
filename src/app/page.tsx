34a72fea77cbf1102df1823bec07a0f2d94be5ed
EC2 Default User <ec2-user@ip-172-31-7-155.ec2.internal>
2025-07-09 02:28:30 +0000

    first commitp3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       
         <ol>
           <li>
-            p2<code>src/app/page.tsx</code>.
+            p3<code>src/app/page.tsx</code>.
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
