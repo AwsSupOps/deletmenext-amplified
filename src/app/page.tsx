ce9ef47c167c137888a4bc4181732f7624fb69f8
EC2 Default User <ec2-user@ip-172-31-7-155.ec2.internal>
2025-07-09 02:16:02 +0000

    first commitnokm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       
         <ol>
           <li>
-            Get started by editing hellonoko <code>src/app/page.tsx</code>.
+            Get started by editing hellonokonm <code>src/app/page.tsx</code>.
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
